4472a125d040bf7b595900a1b5f045986c6332d6
cudjoeab <cudjoeab@gmail.com>
2019-08-15 21:13:05 -0400

    switch

---
+++ pacman-switch.js
@@ -0,0 +1,19 @@
+let ghost = 'Blinky';
+let color;
+
+switch (ghost) {
+    case 'Inky':
+      colour = 'Cyan';
+      break;
+    case 'Blinky':
+      colour = 'Red';
+      break;
+    case 'Pinky':
+      colour = 'Pink';
+      break;
+    case 'Clyde':
+      colour = 'Orange';
+      break;
+  }
+
+console.log(`${ghost} is the colour ${colour}`); 
