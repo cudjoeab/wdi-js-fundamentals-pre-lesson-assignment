a96b9cd286678e6c117c6bf0197180c828228428
cudjoeab <cudjoeab@gmail.com>
2019-08-15 20:36:34 -0400

    pacman objects 2

---
+++ pacman-objects-2.js
@@ -0,0 +1,16 @@
+const ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);      
+
+const ghostName = 'clyde';
+console.log(ghosts[ghostName]); 
+
+// loops 
+
+
+
